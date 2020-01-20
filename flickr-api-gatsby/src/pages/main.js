82e1818301671de8165aebe1e6beaf7dee5a609f
Anthony Salemo <ajssalemo@gmail.com>
2020-01-19 22:54:28 -0500

    created a main page after the landing page, able to pull graphql queries from unsplash now using apollo/graphql

--- flickr-api-gatsby/src/pages/main.js
+++ flickr-api-gatsby/src/pages/main.js
@@ -1,59 +1,25 @@
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
-import { Button, Grid, Typography } from "@material-ui/core"
 import React from "react"
-import styled from "styled-components"
-import { Link } from "gatsby"
+import { useQuery } from "@apollo/react-hooks"
+import { RANDOM_IMAGE_SEARCH } from "../apollo/queries"
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
-const SignInDiv = styled.div`
-    position: absolute;
-    top: 50%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    padding-left: 4em;
-    @media (min-width: 990px) {
-        display: flex;
-        flex-direction: row;
-    }
-`
-const SignInGrid = styled(Grid)`
-    justify-content: center;
-    padding: 4em;
-    @media (min-width: 600px) {
-        display: flex;
-    }
-`
-const LandingpageMainText = styled(Typography)`
-    color: #fff;
-    font-family: "Pacifico"; 
-    font-size: 5em;
-`
-const ButtonGrid = styled(Grid)`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-`
+const Main = () => {
+  const { loading, error, data } = useQuery(RANDOM_IMAGE_SEARCH)
+  if (loading) return "Loading..."
+  if (error) return `Error! ${error.message}`
+  console.log(data)
+  return <div>test</div>
+}
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
-export const SignIn = () => (
-    <SignInDiv>
-        <SignInGrid item>
-            <LandingpageMainText variant="h1">Something like pinterest</LandingpageMainText>
-        </SignInGrid>
-        <ButtonGrid>
-            <Link to="/main" style={{ color: "#fff" }}>Sign in</Link>
-            <Button style={{ color: "#fff" }}>Create account</Button>
-            <Button style={{ color: "#fff" }}>Continue</Button>
-        </ButtonGrid>
-    </SignInDiv>
-)
+export default Main
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
