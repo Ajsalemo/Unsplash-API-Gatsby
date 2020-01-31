f703187cb3d40d97cbcd6ded0ea154caadfa8f8b
Anthony Salemo <ajssalemo@gmail.com>
2020-01-30 23:10:19 -0500

    created an user menu with an avatar profile image, need to square away conditional rending upon logout

--- unsplash-api-gatsby/src/components/mainnavbar.js
+++ unsplash-api-gatsby/src/components/mainnavbar.js
@@ -7,27 +7,18 @@ import React from "react"
 import styled from "styled-components"
 import { MobileNavMenu } from "./mobilenavmenu"
 import { SearchForm } from "./searchform"
-import { logout, login } from "../utils/auth"
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
 const NavbarSitename = styled(Typography)`
   display: none;
-  @media (min-width: 700px) {
+  @media (min-width: 708px) {
     color: #fff;
     font-family: "Pacifico";
     display: initial;
   }
 `
-const MainNavLinks = styled(Grid)`
-  display: none;
-  @media (min-width: 700px) {
-    display: flex;
-    justify-content: space-between;
-  }
-`
-
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
 export const MainNavbar = ({ user }) => (
@@ -40,13 +31,7 @@ export const MainNavbar = ({ user }) => (
       <Link to="/" style={{ textDecoration: "none" }}>
         <NavbarSitename>Something like Unsplash</NavbarSitename>
       </Link>
-      <MainNavLinks item sm={2} md={2} lg={1} xl={1}>
-        <Link to="/main" style={{ color: "#fff", textDecoration: "none" }}>
-          <Typography>Home</Typography>
-        </Link>
-        {user.name ? <Typography onClick={() => logout()}>Sign out, {user.nickname}</Typography> : <Typography onClick={() => login()}>Sign in</Typography>}
-      </MainNavLinks>
-      <MobileNavMenu />
+      <MobileNavMenu user={user} />
     </Toolbar>
   </AppBar>
 )
--- unsplash-api-gatsby/src/components/mobilenavmenu.js
+++ unsplash-api-gatsby/src/components/mobilenavmenu.js
@@ -3,30 +3,64 @@
 
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Button, ClickAwayListener, Grow, MenuItem, MenuList, Paper, Popper } from "@material-ui/core"
+import {
+  Button,
+  Grid,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  Typography,
+  MenuList,
+  Paper,
+  Popper,
+} from "@material-ui/core"
 import { Link } from "gatsby"
-import React, { useEffect, useRef, useState } from "react"
+import React, { Fragment, useEffect, useRef, useState } from "react"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import "react-lazy-load-image-component/src/effects/blur.css"
 import styled from "styled-components"
+import { logout, login } from "../utils/auth"
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 
-const MobileNavDiv = styled.div`
-  display: flex;
-  @media (min-width: 700px) {
-    display: none;
-  }
-`
 const MobileNavPaper = styled(Paper)`
   border: 1px solid #fff;
   border-radius: 0;
   color: #fff;
   background-color: #1e172f;
 `
+const StyledAvatar = styled(LazyLoadImage)`
+  height: 3em;
+  border-radius: 50%;
+  border: 3px solid #fff;
+  &:hover {
+    cursor: pointer;
+  }
+`
+const MediaMenuIcon = styled(FontAwesomeIcon)`
+  color: #fff;
+  size: 3em;
+  display: initial;
+  @media (min-width: 708px) {
+    display: none;
+  }
+`
+const NonMobileNavLinks = styled(Grid)`
+  display: none;
+  @media (min-width: 708px) {
+    display: flex;
+  }
+`
+const ConditionalDesktopLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  padding: 0em 0.4em;
+`
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
 // The below logic was taken straight out of Material-UI's Menu Component example on their site
-export const MobileNavMenu = () => {
+export const MobileNavMenu = ({ user }) => {
   const [open, setOpen] = useState(false)
   const anchorRef = useRef(null)
 
@@ -58,55 +92,74 @@ export const MobileNavMenu = () => {
   }, [open])
 
   return (
-    <MobileNavDiv>
-      <div>
-        <Button
-          ref={anchorRef}
-          aria-controls={open ? "menu-list-grow" : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-        >
-          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} size="2x" />
-        </Button>
-        <Popper
-          open={open}
-          anchorEl={anchorRef.current}
-          role={undefined}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{
-                transformOrigin:
-                  placement === "bottom" ? "center top" : "center bottom",
-              }}
-            >
-              <MobileNavPaper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList
-                    autoFocusItem={open}
-                    id="menu-list-grow"
-                    onKeyDown={handleListKeyDown}
+    <div>
+      <Button
+        ref={anchorRef}
+        aria-controls={open ? "menu-list-grow" : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+      >
+        {user.name ? (
+          <StyledAvatar src={user.picture} />
+        ) : (
+          <MediaMenuIcon icon={faBars} />
+        )}
+      </Button>
+      {!user.name ? (
+        <NonMobileNavLinks>
+          <ConditionalDesktopLink to="/main">
+            <Typography>Home</Typography>
+          </ConditionalDesktopLink>
+          <Button style={{ color: "#fff", padding: "0em" }} onClick={() => login()}>
+            Sign in
+          </Button>
+        </NonMobileNavLinks>
+      ) : null}
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <MobileNavPaper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id="menu-list-grow"
+                  onKeyDown={handleListKeyDown}
+                >
+                  <Link
+                    to="/main"
+                    onClick={handleClose}
+                    style={{ textDecoration: "none", color: "#fff" }}
                   >
-                    <Link
-                      to="/main"
-                      onClick={handleClose}
-                      style={{ textDecoration: "none", color: "#fff" }}
-                    >
-                      <MenuItem onClick={handleClose}>Home</MenuItem>
-                    </Link>
+                    <MenuItem onClick={handleClose}>Home</MenuItem>
+                  </Link>
+                  {/* If a user is logged in, then render these conditional components */}
+                  {user.name ? (
                     <MenuItem onClick={handleClose}>My account</MenuItem>
+                  ) : null}
+                  {user.name ? (
+                    <MenuItem onClick={() => logout()}>Sign out</MenuItem>
+                  ) : (
                     <MenuItem onClick={handleClose}>Sign in</MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </MobileNavPaper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </MobileNavDiv>
+                  )}
+                </MenuList>
+              </ClickAwayListener>
+            </MobileNavPaper>
+          </Grow>
+        )}
+      </Popper>
+    </div>
   )
 }
 
