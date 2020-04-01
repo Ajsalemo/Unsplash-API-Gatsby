e83667aecee61d3b62c93d935ab08fd3ca10479c
Anthony Salemo <ajssalemo@gmail.com>
2020-03-31 22:42:24 -0400

    able to conditionally change the like/delete function depending on whether or not an image is liked

---
+++ unsplash-api-gatsby/src/pages/account.js
@@ -0,0 +1,88 @@
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+import { CircularProgress } from "@material-ui/core"
+import React, { useEffect, useState } from "react"
+import { Footer } from "../components/footer"
+import { LoadingContainer } from "../components/loadingcontainer"
+import { MainNavbar } from "../components/mainnavbar"
+import { MainPageImages } from "../components/mainpageimages"
+import { NoImages } from "../components/noimages"
+import { TotalResultsHeader } from "../components/totalresultsheader"
+import { StyledMainContainer } from "../helpers/styledcomponents"
+import { getProfile, isAuthenticated, login } from "../utils/auth"
+import firebase from "../utils/firebase"
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+const dbUserAccount = firebase.firestore().collection("users")
+
+const getUserAccountImages = (user, setSavedImages, setLoading) => {
+  const savedImagesArray = []
+  setLoading(true)
+  dbUserAccount
+    .doc(user.name)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        const documentResult = doc.data()
+        // If the document exists, loop through the properties within the object
+        for (const property in documentResult) {
+          // This pushes the custom component into the empty savedImagesArray, this also sets the 'src' attribute of the image component to the properties within the firestore document
+          savedImagesArray.push(documentResult[property])
+        }
+        // Invoke the state setting function to set "savedImages" state to the savedImagesArray
+        setLoading(false)
+        setSavedImages(savedImagesArray)
+      } else {
+        console.log("No document")
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+const Account = ({ location }) => {
+  const [savedImages, setSavedImages] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const user = getProfile()
+  useEffect(() => {
+    // If an un authenticated user tries to access this route, then push them to the login page for Auth0
+    if (!isAuthenticated()) {
+      login()
+      return <LoadingContainer />
+    }
+    getUserAccountImages(user, setSavedImages, setLoading)
+  }, [user])
+
+  return (
+    <StyledMainContainer container>
+      <MainNavbar user={user} />
+      <TotalResultsHeader keyword={"Your saved images"} />
+      {savedImages !== null ? (
+        <>
+          <MainPageImages
+            loading={loading}
+            images={savedImages}
+            user={user}
+            location={location.pathname}
+          />
+          {/* If there are no saved images for the user on their account, render this component */}
+          <NoImages savedImages={savedImages} />
+        </>
+      ) : (
+        // While we're waiting for a response to be returned from firebase, show a loading indicator
+        <CircularProgress style={{ color: "#fff" }} />
+      )}
+      <Footer />
+    </StyledMainContainer>
+  )
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+export default Account
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
